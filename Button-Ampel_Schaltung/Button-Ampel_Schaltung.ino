b7c724370f76ff865e5fcc1fc28c298c0ae30dad
Kilian <Kilian@lbgq.de>
2024-07-01 16:07:25 +0200

    Behebung von Fehlern
    erweiterung auf 5 Cases

--- Button-Ampel_Schaltung/Button-Ampel_Schaltung.ino
+++ Button-Ampel_Schaltung/Button-Ampel_Schaltung.ino
@@ -5,7 +5,7 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
-  pinMode(9, INPUT);
+  pinMode(5, INPUT);
 
   pinMode(4, OUTPUT); //GRÜN
   pinMode(3, OUTPUT); //GELB
@@ -19,7 +19,7 @@ void loop() {
     delay(200);
     Serial.println(state);
   } 
-  if(state > 3){
+  if(state > 5){
     state = 0;
     Serial.println(state);
   }
@@ -36,30 +36,43 @@ void loop() {
   case 1:
     digitalWrite(4, LOW);
     digitalWrite(3, HIGH);
+    digitalWrite(2, HIGH);
+    //ist ROT/GELB
+    break;
+
+  case 2:
+    digitalWrite(4, LOW);
+    digitalWrite(3, HIGH);
     digitalWrite(2, LOW);
     //ist GELB
     break;
 
-  case 2:
+  case 3:
     digitalWrite(4, HIGH);
     digitalWrite(3, LOW);
     digitalWrite(2, LOW);
     //ist GRÜN
     break;
-  }
 
-    case 3:
+  case 4:
     digitalWrite(4, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(2, HIGH);
+    //ist GRÜN
+    break;
+
+  case 5:
+    digitalWrite(4, LOW);
     digitalWrite(3, LOW);
     digitalWrite(2, LOW);
-    //ist
+    //ist GRÜN
     break;
-  }
 
   default:
     digitalWrite(4, LOW);
     digitalWrite(3, LOW);
-    digitalWrite(2, HIGH);
+    digitalWrite(2, LOW);
   
     break;
+  }
 }
